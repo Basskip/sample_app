8684c95f4ae44b7362bed9bdc94d1d8ec7e941c9
Cameron Scott <camscottgreen@hotmail.com>
2019-10-10 19:30:47 +1000

    Finish user edit, update, index and destroy actions

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -13,5 +13,9 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "title", full_title("Contact")
     get signup_path
     assert_select "title", full_title("Sign up")
+    log_in_as(users(:michael))
+    get root_path   
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(users(:michael))
   end
 end
